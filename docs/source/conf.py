0e6d284d5f8da53422f9320d78ce7f0f6efa2103
Xinyang Li <lix@lanl.gov>
2022-10-24 23:28:42 -0600

    Update styles of documentations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,10 +59,19 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "navigation_depth": -1,
     "prev_next_buttons_location": "both",
+    "navigation_with_keys": True,
 }
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/custom.css",
+]
+
+# html_logo = "_static/logo.svg"
