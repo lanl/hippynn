abae0f8aaf0057f255cd5c77f0584c7d19e64320
Xinyang Li <lix@lanl.gov>
2022-11-21 17:07:18 -0700

    Fix the dipole node for multi-targets

--- hippynn/layers/physics.py
+++ hippynn/layers/physics.py
@@ -4,6 +4,7 @@ Layers for physical operations
 import warnings
 
 import torch
+from torch import Tensor
 
 from . import pairs
 from . import indexers
@@ -39,7 +40,14 @@ class Dipole(torch.nn.Module):
         super().__init__()
         self.summer = indexers.MolSummer()
 
-    def forward(self, charges, positions, mol_index, n_molecules):
+    def forward(self, charges: Tensor, positions: Tensor, mol_index: Tensor, n_molecules: int):
+        if charges.shape[1] > 1:
+            # charges contain multiple targets, so set up broadcasting
+            charges = charges.unsqueeze(1)
+            positions = positions.unsqueeze(2)
+
+        # shape is (n_atoms, 3, n_targets) in multi-target mode
+        # shape is (n_atoms, 3) in single target mode
         dipole_elements = charges * positions
         dipoles = self.summer(dipole_elements, mol_index, n_molecules)
         return dipoles
@@ -58,7 +66,7 @@ class Quadrupole(torch.nn.Module):
         # charge shape: (atoms,1)
         ri_rj = positions.unsqueeze(1) * positions.unsqueeze(2)
         ri_rj_flat = ri_rj.reshape(-1, 9)  # Flatten to component
-        rsq = (positions ** 2).sum(dim=1).unsqueeze(1)  # unsqueeze over component index
+        rsq = (positions**2).sum(dim=1).unsqueeze(1)  # unsqueeze over component index
         delta_ij = torch.eye(3, device=rsq.device).flatten().unsqueeze(0)  # unsqueeze over atom index
         quad_elements = charges * (ri_rj_flat - (1 / 3) * (rsq * delta_ij))
         quadrupoles = self.summer(quad_elements, mol_index, n_molecules)
