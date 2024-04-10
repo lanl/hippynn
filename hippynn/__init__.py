604a93c0eef9b7b5ed0be6b7724d554a11e9ab08
Nicholas Lubbers <56895592+lubbersnick@users.noreply.github.com>
2024-04-10 16:20:06 -0600

    Deploy versioneer 0.29 for python 3.12 compatibility (#64)
    * Deploy versioneer 0.29
    
    * avoid syntax error warning when loading
    
    The \pi in the doc string triggers the warning
     SyntaxWarning: invalid escape sequence '\p'
    
    ---------
    
    Co-authored-by: Sakib Matin <83463357+sakibmatin@users.noreply.github.com>

--- hippynn/__init__.py
+++ hippynn/__init__.py
@@ -4,9 +4,8 @@ The hippynn python package.
 
 """
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 # Configurational settings
 from ._settings_setup import settings
