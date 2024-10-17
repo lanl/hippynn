524a4c09715746e186d59e1462fe9fce5882972b
Emily Shinkle <eshinkle@lanl.gov>
2024-10-17 13:21:24 -0600

    fix StressForceNode

--- hippynn/interfaces/ase_interface/calculator.py
+++ hippynn/interfaces/ase_interface/calculator.py
@@ -339,9 +339,9 @@ class HippynnCalculator(Calculator): # Calculator inheritance required for ASE M
         cell = torch.as_tensor(self.atoms.cell.array)  # ExternalNieghbors doesn't take batch index
         # Get pair first and second from neighbors list
 
-        pair_first = torch.as_tensor(self.nl.nl.pair_first,dtype=torch.long)
-        pair_second = torch.as_tensor(self.nl.nl.pair_second,dtype=torch.long)
-        pair_shiftvecs = torch.as_tensor(self.nl.nl.offset_vec,dtype=torch.long)
+        pair_first = torch.as_tensor(self.nl.nl.pair_first,dtype=torch.long).unsqueeze(0)
+        pair_second = torch.as_tensor(self.nl.nl.pair_second,dtype=torch.long).unsqueeze(0)
+        pair_shiftvecs = torch.as_tensor(self.nl.nl.offset_vec,dtype=torch.long).unsqueeze(0)
 
         # This order must be synchronized with function setup_ase_graph above
         inputs = species, positions, cell, pair_first, pair_second, pair_shiftvecs
