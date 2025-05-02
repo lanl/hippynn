116323241e86367d6f09c70cf429797ac0d86ebb
Emily Shinkle <eshinkle@lanl.gov>
2025-05-02 08:56:28 -0600

    add pbc wrapping option to cg functions

--- hippynn/cgtools/coarse_grain.py
+++ hippynn/cgtools/coarse_grain.py
@@ -118,17 +118,25 @@ def coarse_grain_all(values, atom_to_bead, coarse_grain_one, masses=None, cells=
 
 # --- Mapping functions ---
 
-def cg_one_center_of_mass_pbc(bead_values, bead_masses, frame_cell):
+def cg_one_center_of_mass_pbc(bead_values, bead_masses, frame_cell, wrap_into_cell=True):
     """Position center of mass using PBC"""
     validate_diagonal_cell(cell=frame_cell)
     mic_dists = find_mic(bead_values[1:] - bead_values[0], cell=frame_cell)
-    return np.sum(mic_dists * bead_masses[1:, None], axis=0) / bead_masses.sum() + bead_values[0]
+    com = np.sum(mic_dists * bead_masses[1:, None], axis=0) / bead_masses.sum() + bead_values[0]
+    if wrap_into_cell:
+        return com % np.diag(frame_cell)
+    else:
+        return com
 
-def cg_one_center_of_geometry_pbc(bead_values, frame_cell):
+def cg_one_center_of_geometry_pbc(bead_values, frame_cell, wrap_into_cell=True):
     """Position center of geometry using PBC"""
     validate_diagonal_cell(cell=frame_cell)
     mic_dists = find_mic(bead_values[1:] - bead_values[0], cell=frame_cell)
-    return np.sum(mic_dists, axis=0) / bead_values.shape[0] + bead_values[0]
+    cog = np.sum(mic_dists, axis=0) / bead_values.shape[0] + bead_values[0]
+    if wrap_into_cell:
+        return cog % np.diag(frame_cell)
+    else:
+        return cog
 
 def cg_one_mass_weighted_average(bead_values, bead_masses):
     """General mass-weighted average (eg. for velocities when using COM position mapping)"""
