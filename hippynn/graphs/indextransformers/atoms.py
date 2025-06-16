8042df71949ef6b1e3f74ce22d6410a6da9b435b
Nicholas Lubbers <nlubbers@lanl.gov>
2025-06-16 14:02:36 -0600

    add hint nodes to search

--- hippynn/graphs/indextransformers/atoms.py
+++ hippynn/graphs/indextransformers/atoms.py
@@ -15,23 +15,25 @@ from ..nodes.inputs import SpeciesNode
 def idx_molatom_atom(node, hints=None):
     purpose = "auto-generating indexing for {}".format(node)
 
+    search_nodes = set([node,*hints])
+
     funrel = find_unique_relative  # Abbreviation because we so many calls in this function.
     if node.origin_node is None:
         # If we are auto-indexing in the model graph, it should be easy.
         # species = funrel(node, SpeciesNode,
         #                 why_desc=purpose)
-        pidxer = funrel(node, PaddingIndexer, why_desc=purpose)
+        pidxer = funrel(search_nodes, PaddingIndexer, why_desc=purpose)
         idx_debprint("Using reindexer for ", node)
 
     else:
         # If we are auto-indexing in the loss graph, it gets a bit complicated.
-
+        search_nodes = set(n.origin_node for n in search_nodes)
         # The species we link to will be the true version of species, that is, the node where
-        species = funrel(node.origin_node, SpeciesNode, why_desc=purpose).true
+        species = funrel(search_nodes, SpeciesNode, why_desc=purpose).true
         try:
             encoder = funrel(species, OneHotEncoder, why_desc=purpose)
         except NodeOperationError as ne:
-            idx_debprint("Creating new ENCODER")
+            idx_debprint("Creating new encoder in loss graph.")
             # If this fails, something bad has happened -- the loss graph is trying to do something not defined by the
             # model graph
             origin_encoder = funrel(species.origin_node, OneHotEncoder, why_desc=purpose)
