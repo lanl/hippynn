d15a32e02d002a0c99bc97777ff6675e357302e3
Nicholas Lubbers <nlubbers@lanl.gov>
2025-06-30 15:41:50 -0600

    formatter

--- hippynn/_deprecations.py
+++ hippynn/_deprecations.py
@@ -7,48 +7,46 @@ import warnings, contextlib, functools
 
 class HippynnNameDeprecation(DeprecationWarning):
     def __init__(self, deprecations, old_module=None, new_module=None):
-        
+
         deprecations = dict(deprecations)
-        
+
         if not deprecations:
             raise ValueError("Arguments are required to be non-empty.")
-        
+
         if old_module is not None:
             if not isinstance(old_module, str):
                 old_module = old_module.__name__
-            deprecations = {f"{old_module}.{k}":v for k,v in deprecations.items()}
+            deprecations = {f"{old_module}.{k}": v for k, v in deprecations.items()}
         if new_module is not None:
             if not isinstance(new_module, str):
                 new_module = new_module.__name__
-            deprecations = {k:f"{new_module}.{v}" for k,v in deprecations.items()}
+            deprecations = {k: f"{new_module}.{v}" for k, v in deprecations.items()}
 
-        for k,v in deprecations.copy().items():
+        for k, v in deprecations.copy().items():
             if not isinstance(v, str):
                 try:
                     vname = v.__qualname__
                 except:
                     vname = repr(v)
                 deprecations[k] = f"{v.__module__}.{vname}"
-        
+
         self.deprecations = deprecations
 
         message = ["The following hippynn names have been deprecated, use replacements:"]
-        for k,v in deprecations.items():
+        for k, v in deprecations.items():
             message.append(f"\t{k} -> {v}")
         message.append("Use HIPPYNN_DEPRECATION_WARNINGS=ignore to disable this warning.")
-        
+
         message = "\n".join(message)
 
         super().__init__(message)
 
-
     @classmethod
     def from_single(cls, old, new, **kwargs):
-        deprecations = {old:new}
+        deprecations = {old: new}
         return cls(deprecations, **kwargs)
 
-
-    @classmethod 
+    @classmethod
     def from_list(cls, *args, **kwargs):
 
         new_items = {}
@@ -58,18 +56,16 @@ class HippynnNameDeprecation(DeprecationWarning):
                     rewarn(a)
                     continue
                 else:
-                    a = a.message            
+                    a = a.message
             new_items.update(a.deprecations)
         if not new_items:
             return None
         return cls(deprecations=new_items, **kwargs)
 
+
 def rewarn(*message_list):
     for m in message_list:
-        warnings.warn_explicit(m.message,
-                               m.category,
-                               m.filename,
-                               m.lineno)
+        warnings.warn_explicit(m.message, m.category, m.filename, m.lineno)
 
 
 @contextlib.contextmanager
@@ -79,16 +75,15 @@ def handle_deprecations(stacklevel=0):
             warnings.simplefilter("always")
             yield
 
-        
         new_warning = HippynnNameDeprecation.from_list(*recorded)
         if new_warning is not None:
-            warnings.warn(new_warning, stacklevel=2+stacklevel)
+            warnings.warn(new_warning, stacklevel=2 + stacklevel)
 
     elif settings.DEPRECATION_WARNINGS == "simple":
         with warnings.catch_warnings(record=True) as recorded:
             warnings.simplefilter("always")
             yield
-        
+
         found_deprecations = False
         for m in recorded:
             if not isinstance(m.message, HippynnNameDeprecation):
@@ -96,28 +91,32 @@ def handle_deprecations(stacklevel=0):
             else:
                 found_deprecations = True
         if found_deprecations:
-            warnings.warn("Deprecated hippynn variable names were encountered. If you encounter this message while "
-                          "loading a checkpoint, you can disable this message by saving it back again or "
-                          "using HIPPYNN_DEPRECATION_WARNINGS=ignore .")        
+            warnings.warn(
+                "Deprecated hippynn variable names were encountered. If you encounter this message while "
+                "loading a checkpoint, you can disable this message by saving it back again or "
+                "using HIPPYNN_DEPRECATION_WARNINGS=ignore ."
+            )
 
     elif settings.DEPRECATION_WARNINGS == "ignore":
         with warnings.catch_warnings(record=True) as recorded:
-            warnings.simplefilter("ignore",  HippynnNameDeprecation)
+            warnings.simplefilter("ignore", HippynnNameDeprecation)
             yield
     else:
         raise ValueError(f"unknown setting: {settings.DEPRECATION_WARNINGS}")
 
     return
-    
+
 
 def bundles_deprecated_warnings(wrapped=None, stacklevel=None):
-    if wrapped is None:      
+    if wrapped is None:
         return functools.partial(bundles_deprecated_warnings, stacklevel=stacklevel)
-    
+
     if stacklevel is None:
         stacklevel = 0
+
     @functools.wraps(wrapped)
     def inner(*args, **kwargs):
         with handle_deprecations(stacklevel=stacklevel):
-            return wrapped(*args,**kwargs)
+            return wrapped(*args, **kwargs)
+
     return inner
