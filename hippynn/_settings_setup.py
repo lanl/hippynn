441ba2e2431aab2e112b7bcce60a487ebe5514d1
Nicholas Edward Lubbers <nlubbers@lanl.gov>
2025-09-15 12:01:05 -0600

    remove distutils dependency

--- hippynn/_settings_setup.py
+++ hippynn/_settings_setup.py
@@ -12,7 +12,6 @@ import os
 import configparser
 from typing import Union
 
-from distutils.util import strtobool
 from types import SimpleNamespace
 from functools import partial
 
@@ -101,8 +100,20 @@ def kernel_handler(kernel_string):
 def bool_or_strtobool(key: Union[bool, str]):
     if isinstance(key, bool):
         return key
+    elif isinstance(key,str):
+        # Boolean states 
+        return {
+            '1': True,    
+            'yes': True,
+            'true': True,
+            'on': True,
+            '0': False,
+            'no': False,
+            'false': False,
+            'off': False,
+            }[key.casefold()]
     else:
-        return strtobool(key)
+        raise ValueError(f"Invalid value {key} of type {type(key)}. Truth-ish string or boolean is required.")
 
 def deprecation_handler(deprecation_string):
     deprecation_string = deprecation_string.lower()
